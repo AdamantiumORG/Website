5dc1f6982227a84d52fa92e2a9bb8477ce034806
AdmerPRO <144557744+AdmerPRO@users.noreply.github.com>
2026-09-14 19:06:22 +0200

    feat: add simple html/css website

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use axum::{Router, response::Html, routing::get};
 use std::env;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
+
     let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
     let addr = format!("0.0.0.0:{}", port);
 
-    let app = Router::new().route("/", get(home));
+    let app = Router::new()
+        .route("/", get(home))
+        .nest_service("/static", ServeDir::new("static"));
 
     let listener = tokio::net::TcpListener::bind(&addr)
         .await
@@ -21,5 +25,5 @@ async fn main() {
 }
 
 async fn home() -> Html<&'static str> {
-    Html("<h1>Hello World!</h1>")
+    Html(include_str!("../static/index.html"))
 }
